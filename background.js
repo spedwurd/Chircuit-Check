af6e4a606a608b98d6ed7ab046c56211e6454228
edzhang10 <edzhang10@gmail.com>
2025-10-04 14:39:41 -0400

    First commit we are so cooked

---
+++ background.js
@@ -0,0 +1,8 @@
+document.getElementById('clickMe').addEventListener('click', async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => alert('Hello from your extension!')
+  });
+});
+ff
